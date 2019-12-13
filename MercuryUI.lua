f7d1f42b73ed695cd14a7bb60602a1b7d481f0b4
PlutonusDev <plutonus.stream@gmail.com>
2019-12-13 19:41:56 +1000

    Fixes.

--- MercuryUI.lua
+++ MercuryUI.lua
@@ -235,13 +235,13 @@ Mercury={_instances={}} do
                                         },
                                         [New]=function(this)
                                             this.MouseButton1Click:connect(function()
-                                                if config._link._link._cpage == config.title then return end
-                                                config._link._link._cpage = config.title
-                                                for k,v in pairs(config._link._link._pages) do
+                                                if config._link._cpage == config.title then return end
+                                                config._link._cpage = config.title
+                                                for k,v in pairs(config._link._pages) do
                                                     if k~=config.title then
-                                                        if config._link._link._pages[k]._body then config._link._link._pages[k]._body.Visible = false end
+                                                        if config._link._pages[k]._body then config._link._pages[k]._body.Visible = false end
                                                     else
-                                                        if config._link._link._pages[k]._body then config._link._link._pages[k]._body.Visible = true end
+                                                        if config._link._pages[k]._body then config._link._pages[k]._body.Visible = true end
                                                     end
                                                 end
                                                 if config._link._body then config._link._body.Visible = true end
@@ -251,7 +251,7 @@ Mercury={_instances={}} do
                                 }
                                 New"Frame"{
                                     Name = config.title,
-                                    Parent = config._link._link._inst.MainFrame.Body,
+                                    Parent = config._link._inst.MainFrame.Body,
                                     Size = UDim2.new(1, 0, 1, 0),
                                     BackgroundTransparency = 1,
                                     Visible = false,
@@ -300,7 +300,6 @@ Mercury={_instances={}} do
                                     },
                                     [New]=function(this)
                                         this.MouseButton1Click:connect(function()
-                                            for k,v in pairs(config._link) do print(k,v)end
                                             if config._link._cpage == config.title then return end
                                             config._link._cpage = config.title
                                             for k,v in pairs(config._link._pages) do
